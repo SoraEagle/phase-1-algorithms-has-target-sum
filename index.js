20bb222f0a89905fa5d0cbe6409401eae5d7f49b
Matthew <phillips.matthew829@gmail.com>
2021-11-30 14:48:35 -0600

    commit tree cadb8e3924d772099e3ac2a58538cbb24de2e202

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here:
-  for(const num1 of array){ //Primary/Parent loop for 1st # for finding sum.
-    for(const num2 of array){ //Nested/Child loop for 2nd # for finding sum.
-      //Ensure that the num1 doesn't get added to itself.
-      if((num1 + num2) === target) return true; //Compare each sum found to target Integer.
-    }
-    return false; //No possible sums match the target Integer.
+function hasTargetSum(array, target){
+  // Write algorithm here:
+  for(let num1 = 0; num1 < array.length; num1++){ //Primary/Parent loop for 1st # for finding sum.
+    for(let num2 = num1 + 1; num2 < array.length; num2++){ //Nested/Child loop for 2nd # for finding sum.
+      //Ensure that num1 doesn't get added to itself.
+      if((array[num1] + array[num2]) === target) return true; //Compare each sum found to target Integer.
+    } //No possible sums match the target Integer.
   }
+  return false;
 }
 
 /* 
   The Big O time complexity of the function:
-    Time Complexity: O(?)
-      Steps included:
-        The function itself (0 steps),
-        No methods used on the Array itself (0 steps),
-        Using two numbers to find a sum 
-          requiring two (for of) loops (n^2 steps),
-      Total Time Complexity: O().
-    Function's summarized Time Complexity: O(n^2).
-*/
-
+    Time Complexity: O(n^2).*/
 /* 
   Pseudocode:
   Create hasTargetSum function that recieves, as arguments,
@@ -28,18 +19,19 @@ function hasTargetSum(array, target) {
   In hasTargetSum:
     Compare the sum of two #s:
       If, any pair of #s in the array adds up to target #, return true.
-      Else if, all possible sums have not been looked at, continue looking.
-      Else, if all possible sums != target Integer, return false.
-      */
-
+      Else, if all possible sums != target Integer, return false.*/
 /*
   Written explanation of solution:
-  //
-*/
+  This is the most optimized that I could make the solution for this function for now.  As it is 
+  using two for loops, one of which is nested, the Time Complexity,in Big O notation, is O(n^2).  
+  This is the summarized notation for the Time Complexity, but in depth, since the array is being 
+  referenced in each for loop (total of two loops),and one is nested in the other, normally it 
+  would be just O(n^2), but with only the first loop iterating fully through the array, and with 
+  each new iteration becoming smaller, it is simply estimated to be O(n^2).*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  // add custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
   console.log("");
